cf8689244017125b9aea6164a638bad649102ff2
MauricioSerrato <mserato99@gmail.com>
2022-09-28 18:10:56 -0500

    auth working, very happpy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts
   root 'posts#index'
   get 'home/about'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
