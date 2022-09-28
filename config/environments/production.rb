aa90e4cc417991dba85af8f869d2b47374e01a0e
MauricioSerrato <mserato99@gmail.com>
2022-09-28 17:00:01 -0500

    added config action in production and development in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://intense-spire-56626.herokuapp.com/' }
+
+
 end
