4bc96b8512a0961c42117e571237d97f941e45ba
MauricioSerrato <mserato99@gmail.com>
2022-10-02 17:11:33 -0500

    dontbreak pleasee

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
+
+  
   root 'posts#index'
   get 'home/about'
+  get 'home/index'
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
